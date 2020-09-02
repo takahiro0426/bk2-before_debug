a54bc8972fd72aa5bd040ce8c203c7b25863368e
vagrant <vagrant@localhost.localdomain>
2020-09-02 11:37:53 +0000

    [Finish] bk2-before_debug
    second problem

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,55 @@
+class BooksController < ApplicationController
+before_action :authenticate_user!
+  def show
+    @book = Book.find(params[:id])
+    @new_book = Book.new
+  end
+
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def create
+    @book = current_user.books.new(book_params)
+    if @book.save
+      redirect_to book_path(@book), notice: "You have created book successfully."
+    else
+      @books = Book.all
+      render 'index'
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+    if @book.user == current_user
+      render :edit
+    else
+      redirect_to books_path
+    end
+  end
+
+
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "You have updated book successfully."
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
